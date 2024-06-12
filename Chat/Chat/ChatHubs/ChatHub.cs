4cdfdbcbf03de323289e2c553598206f48992906
Cavit Selman <tetikci462@hotmail.com>
2024-06-12 23:53:48 +0300

    Chathub and ChatModel were created, SignalR package was installed.

---
+++ Chat/Chat/ChatHubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.ChatHubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string userName, string Message, DateTime date)        
+        => await Clients.All.SendAsync("ReceiveMessage", userName, Message, date);
+        
+    }
+}
---
+++ ChatModels/Chat.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatModels
+{
+    public class Chat
+    {
+        [Required]
+        public string? Message { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
